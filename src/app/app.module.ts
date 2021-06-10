b0b550f11c81f113a58da4817d708abae38e23c4
tusharkumarkanojia <tushar@thb.co.in>
2021-06-10 14:59:25 +0530

    [NEW] add media capture plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CallNumber } from '@ionic-native/call-number/ngx';
+import { MediaCapture } from '@ionic-native/media-capture/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },CallNumber],
+  providers: [MediaCapture,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },CallNumber],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CallNumber } from '@ionic-native/call-number/ngx';
+import { MediaCapture} from '@ionic-native/media-capture/ngx';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +9,25 @@ import { CallNumber } from '@ionic-native/call-number/ngx';
 })
 export class HomePage {
 
-  constructor( private callNumber: CallNumber) {}
+  constructor( private callNumber: CallNumber, private mediaCapture: MediaCapture) {}
 
   callNow(number) {
     this.callNumber.callNumber(number, true)
-      .then(res => console.log('Launched dialer!', res))
+      .then(res =>  {
+        console.log('Launched dialer!', res)
+        this.startRecording();
+      })
       .catch(err => console.log('Error launching dialer', err));
   }
 
+  startRecording() {
+    this.mediaCapture.captureAudio().then((res) => {
+      console.log('media is being captureed');
+    }).catch((error) => {
+      console.log('error while capturing audio');
+    })
+  }
+
+
+ 
 }
